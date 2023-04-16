255feea6d5c4bdb8c236d1d15a109a560ec7b3e6
shadowrunner11 <pieroramirez810@gmail.com>
2023-04-15 21:36:52 -0500

    :recycle: interfaces migrated of file

--- src/data/mongo/index.ts
+++ src/data/mongo/index.ts
@@ -1,26 +1,25 @@
 import mongoose from "mongoose";
 import { MONGO_URI } from "../../config";
 
+export class Conextion {
+  private static instace: Conextion;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private constructor() {}
 
-export class Conextion{
-  private static instace: Conextion
-  private constructor(){}
-
-  static getConexion(){
-    Conextion.instace ??= new Conextion()
-    return Conextion.instace
+  static getConexion() {
+    Conextion.instace ??= new Conextion();
+    return Conextion.instace;
   }
 
-  async connect(){
-    await mongoose.connect(MONGO_URI)
+  async connect() {
+    await mongoose.connect(MONGO_URI);
   }
 }
 
+mongoose.connection.addListener("error", (error) => {
+  console.log(error);
+});
 
-mongoose.connection.addListener('error', error=>{
-  console.log(error)
-})
-
-mongoose.connection.once('open', ()=>{
-  console.log('conexion exitosa')
-})
+mongoose.connection.once("open", () => {
+  console.log("conexion exitosa");
+});
--- test/integration/validationSchemas.test.ts
+++ test/integration/validationSchemas.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test } from "@jest/globals";
 import supertest from "supertest";
 import { server } from "../../src/server";
+
 describe("validation suite", () => {
   test("create developer schema validation", async () => {
     const result = await supertest(server)
@@ -10,7 +11,6 @@ describe("validation suite", () => {
         descriptions: "adsad",
       });
 
-    console.log(result.body.errors);
     expect(result.statusCode).toBe(400);
     expect(
       result.body.errors?.find(({ path }) => path === "descriptions")
